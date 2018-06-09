2fe52717ac7035ba4ba0cf3dfec769e87a0f1455
Sylvain Estevez <sylvain@luxe.com>
2018-06-09 17:33:55 +0200

    :construction: Spring cleaning

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Collection } from './classes/collection';
-export { ICollection } from './interfaces/collection';
-export { ICollectionConstructor } from './interfaces/collection-constructor';
--- dist/interfaces/collection-constructor.d.ts
+++
@@ -1,4 +0,0 @@
-import { ICollection } from './collection';
-export interface ICollectionConstructor<T> {
-    new (items?: T[]): ICollection<T>;
-}
